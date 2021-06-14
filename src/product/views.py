5c9f98ba5e7d5dbd2b011eaa6d73089d557e8d02
Manas Acharyya <manas.acharyya.nits.cse18@gmail.com>
2021-06-14 14:39:12 +0530

    Cleaned Repo

--- src/product/views.py
+++
@@ -1,9 +0,0 @@
-from django.http.response import HttpResponse
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-
-
-def product_view(*args, **kwargs):
-    return HttpResponse("<h1>Hello Products</h1>")
